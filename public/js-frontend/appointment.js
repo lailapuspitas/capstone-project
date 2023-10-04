a7a527db7f6fcd00bde220d6197f9b0693d51e8d
lailapuspitas <lailaaapspta@gmail.com>
2023-10-04 09:31:44 +0700

    final integration of frontend dan backend

--- public/js-frontend/appointment.js
+++ public/js-frontend/appointment.js
@@ -1,124 +1,54 @@
 const API_BASE_URL = "https://health-care-production.up.railway.app";
 
-document
-  .querySelector(".appointment-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
+async function addNewAppointment() {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const date = document.getElementById("date").value;
+  const time = document.getElementById("time").value;
+  const categories = document.getElementById("service").value;
+  const complaint = document.getElementById("complaint").value;
 
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const date = document.getElementById("date").value;
-    const time = document.getElementById("time").value;
-    const service = document.getElementById("service").value;
-    const complaint = document.getElementById("complaint").value;
+  const appointmentData = {
+    name,
+    email,
+    date,
+    time,
+    categories,
+    complaint,
+  };
 
-    const appointmentData = {
-      name: name,
-      email: email,
-      date: date,
-      time: time,
-      service: service,
-      complaint: complaint,
-    };
-
-    fetch(`${API_BASE_URL}/appointment`, {
+  try {
+    const response = await fetch(`${API_BASE_URL}/appointment`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(appointmentData),
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        alert("Appointment added successfully");
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-        alert("There was an error adding the appointment");
-      });
-  });
+    });
 
-// function addAppointment() {
-//   // URL endpoint dari API Anda
-//   const API_BASE_URL = "https://health-care-production.up.railway.app";
+    if (response.status === 201) {
+      const responseData = await response.json();
+      displayAppointmentData(responseData.data);
+    } else {
+      console.error("Failed to create appointment.");
+    }
+  } catch (error) {
+    console.error("Error sending data: " + error);
+  }
+}
 
-//   // Mengambil nilai dari form
-//   const name = document.getElementById("name").value;
-//   const email = document.getElementById("email").value;
-//   const date = document.getElementById("date").value;
-//   const time = document.getElementById("time").value;
-//   const service = document.getElementById("service").value;
-//   const complaint = document.getElementById("complaint").value;
+function displayAppointmentData(data) {
+  document.getElementById("hasil-name").textContent = data.name;
+  document.getElementById("hasil-email").textContent = data.email;
+  document.getElementById("hasil-date").textContent = data.date;
+  document.getElementById("hasil-time").textContent = data.time;
+  document.getElementById("hasil-symptom").textContent = data.categories;
+  document.getElementById("hasil-medcomplaint").textContent = data.complaint;
+}
 
-//   // Membuat objek appointment baru
-//   const newAppointment = {
-//     name: name,
-//     email: email,
-//     date: date,
-//     time: time,
-//     service: service,
-//     complaint: complaint,
-//   };
-
-//   // Mengirim data ke API
-//   fetch(`${API_BASE_URL}/appointment`, {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify(newAppointment),
-//   })
-//     .then((response) => response.json())
-//     .then((data) => console.log(data))
-//     .catch((error) => {
-//       console.error("Error:", error);
-//     });
-// }
-
-// const API_BASE_URL = "https://health-care-production.up.railway.app";
-
-// async function addAppointment() {
-//   const AppointmentName = document.getElementById("name").value;
-//   const AppointmentEmail = document.getElementById("email").value;
-//   const AppointmentDate = document.getElementById("date").value;
-//   const AppointmentTime = document.getElementById("time").value;
-//   const AppointmentCategories = document.getElementById("service").value;
-//   const AppointmentComplaint = document.getElementById("complaint").value;
-
-//   try {
-//     const response = await fetch(`${API_BASE_URL}/appointment`, {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({
-//         name: AppointmentName,
-//         email: AppointmentEmail,
-//         date: AppointmentDate,
-//         time: AppointmentTime,
-//         categories: AppointmentCategories,
-//         complaint: AppointmentComplaint,
-//       }),
-//     });
-
-//     if (response.ok) {
-//       // Data berhasil dikirim
-//       const data = await response.json();
-//       // Tampilkan data atau lakukan apa yang diperlukan
-//     } else {
-//       // Permintaan tidak berhasil
-//       const errorMessage = await response.json();
-//       document.getElementById("error-message").textContent =
-//         errorMessage.message;
-//     }
-//   } catch (error) {
-//     console.error("Error adding appointment:", error);
-//     document.getElementById("error-message").textContent =
-//       "Failed to add appointment.";
-//   }
-// }
+// Menghubungkan fungsi addNewAppointment() ke form submit
+const appointmentForm = document.getElementById("appointment-form");
+appointmentForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  addNewAppointment();
+});
